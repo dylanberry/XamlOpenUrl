98bc1eef7415c70d34fe969a4b00ba78f5217c5d
Dylan Berry <dylanberry@gmail.com>
2019-12-05 18:16:42 -0500

    Initial spike

---
+++ XamlOpenUrl/Extensions/OpenUrlExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamlOpenUrl.Extensions
+{
+    [ContentProperty(nameof(Name))]
+    public class OpenUrlExtension : BindableObject, IMarkupExtension<ICommand>, ICommand
+    {
+        public static readonly BindableProperty NameProperty =
+            BindableProperty.Create(nameof(Name), typeof(string), typeof(OpenUrlExtension), null);
+
+        public string Name
+        {
+            get => (string)GetValue(NameProperty);
+            set => SetValue(NameProperty, value);
+        }
+
+        protected internal bool IsNavigating { get; private set; }
+
+        public bool CanExecute(object parameter) => !IsNavigating;
+
+        public event EventHandler CanExecuteChanged;
+
+        public async void Execute(object parameter)
+        {
+            IsNavigating = true;
+            try
+            {
+                RaiseCanExecuteChanged();
+                var url = parameter as string ?? Name;
+                await HandleNavigation(url);
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
+            finally
+            {
+                IsNavigating = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        protected virtual void Log(Exception ex)
+        {
+            Xamarin.Forms.Internals.Log.Warning("Warning", $"{GetType().Name} threw an exception");
+            Xamarin.Forms.Internals.Log.Warning("Exception", ex.ToString());
+        }
+
+        protected virtual Task HandleNavigation(string url) => Launcher.OpenAsync(url);
+
+        protected void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public ICommand ProvideValue(IServiceProvider serviceProvider) => this;
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) =>
+            ProvideValue(serviceProvider);
+    }
+}
---
+++ XamlOpenUrl/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamlOpenUrl
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public string Url => "https://dylanberry.com";
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
